d411b4d522b0569604f1bc4ca6175fcf674ebbc0
DeveloperYun <yhb7588@naver.com>
2023-04-24 11:13:23 +0900

    setting

--- capstone2/backend/backend/urls.py
+++ capstone2/backend/backend/urls.py
@@ -1,21 +1,21 @@
-"""backend URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+import mimetypes
+mimetypes.add_type("application/javascript", ".js", True)
+
+# MEDIA_URL로 시작되는 요청이 오면 settings.MEDIA_ROOT 에서 파일을 찾아서 서빙한다.
+# 디버그 옵션이 거짓이라면 아래 static은 빈 리스트를 반환
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    import debug_toolbar
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ]
