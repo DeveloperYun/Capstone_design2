779183420fc597cc26421a2471d2300265e59bf0
DeveloperYun <yhb7588@naver.com>
2023-05-04 13:56:01 +0900

    리액트 대비 cors whitelist

--- capstone2/backend/backend/settings/dev.py
+++ capstone2/backend/backend/settings/dev.py
@@ -7,3 +7,7 @@ INSTALLED_APPS += [
 MIDDLEWARE = ["debug_toolbar.middleware.DebugToolbarMiddleware",] + MIDDLEWARE
 
 INTERNAL_IPS = ["127.0.0.1"] #debug 툴바를 허용할 ip목록
+
+
+CORS_ORIGIN_WHITELIST = ["http://localhost:3000"]
+CORS_ALLOW_CREDENTIALS = True
