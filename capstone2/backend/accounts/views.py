55894b55397a5d501dcae3a5d4bce09c148fd99e
DeveloperYun <yhb7588@naver.com>
2023-05-05 01:25:17 +0900

    회원가입 폼 구현 완료

--- capstone2/backend/accounts/views.py
+++ capstone2/backend/accounts/views.py
@@ -1,3 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from rest_framework.generics import CreateAPIView
+from .serializers import SignupSerializer
 
-# Create your views here.
+#회원가입 뷰
+#createapiview 이므로 get 메소드 허용X
+class SignupView(CreateAPIView):
+    model = get_user_model()
+    serializer_class = SignupSerializer
+    Permission_classes = [
+        AllowAny,#실제로 회원가입할 때는 로그인은 되며 ㄴ안되니까
+    ]
