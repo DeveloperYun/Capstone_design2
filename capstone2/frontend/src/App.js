2eb2f26c2f807c068e5c4fd261d47f520726ba8e
DeveloperYun <yhb7588@naver.com>
2023-05-05 23:22:55 +0900

    frontend 임시

--- capstone2/frontend/src/App.js
+++ capstone2/frontend/src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import React from "react";
+import "App.css";
+import PostList from "PostList";
 
 function App() {
   return (
     <div>
-      hello
+        <PostList />
     </div>
   );
 }
---
+++ capstone2/frontend/src/pages/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Root(){
+    return <div>최상위 component</div>
+}
+
+export default Root;
