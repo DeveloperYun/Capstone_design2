5edafec437ca29c07eeb9642febf0d5cd40cd5bb
DeveloperYun <yhb7588@naver.com>
2023-05-08 11:47:50 +0900

    antd를 적용한 회원가입 폼

---
+++ capstone2/frontend/src/pages/accounts/Signup.js
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import { SmileOutlined, FrownOutlined } from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
+import { Form, Input, Button, notification } from "antd";
+
+export default function Signup() {
+    const history = useNavigate();
+    const [fieldErrors, setFieldErrors] = useState({});
+    
+    const onFinish = values => {
+        async function fn(){
+            const {username, password} = values;
+
+            setFieldErrors({});
+
+            const data = {username, password};
+            try{
+                await Axios.post("http://localhost:8000/accounts/signup/", data);
+                notification.open({
+                    message: "회원가입을 축하합니다",
+                    description: "로그인 페이지로 이동합니다",
+                    icon: <SmileOutlined style={{color: "#10Bee9"}}/>
+                });
+
+                history("/accounts/login/");
+            }
+            catch(error){
+                if(error.response){
+
+                    notification.open({
+                        message: "회원가입 실패",
+                        description: "아이디 및 비밀번호를 다시 확인해주세요",
+                        icon: <FrownOutlined style={{color: "#ff3333"}}/>
+                    })
+
+                    const {data: fieldsErrorMessage} = error.response;
+
+                    setFieldErrors(
+                        Object.entries(fieldsErrorMessage).reduce((acc, [fieldName, errors])=>{
+                            acc[fieldName] = {
+                                validateStatus: "error",
+                                help: errors.join(" "),
+                            }
+                            return acc;
+                          }, 
+                        {}
+                        )
+                    );
+                }
+            }
+        }
+        fn();
+    }
+    return (
+       <Form
+        {...layout}
+        name="basic"
+        initialValues={ { remember: true }}
+        onFinish={onFinish}
+       >
+            <Form.Item
+            label="Username"
+            name="username"
+            rules={[
+                {
+                required: true,
+                message: '5글자 이상 입력하세요',
+                min : 5,
+                },
+            ]}
+            hasFeedback
+            {...fieldErrors.username}
+            >
+            <Input />
+            </Form.Item>
+
+            <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+                {
+                required: true,
+                message: 'Please input your password!',
+                },
+            ]}
+            {...fieldErrors.password}
+            >
+            <Input.Password />
+            </Form.Item>
+
+            <Form.Item {...tailLayout}>
+                <Button type="primary" htmlType="submit">
+                    Submit
+                </Button>
+            </Form.Item>
+        </Form> 
+    );
+}
+
+const layout = {
+    labelCol: { span: 8},
+    wrapperCol: { span: 16}
+
+};
+
+const tailLayout = {
+    wrapperCol: { offset: 8, span: 16}
+};
