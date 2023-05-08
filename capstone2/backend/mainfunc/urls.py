71069e3b42dbdc0da8bdd268a7e3391df0e700dd
DeveloperYun <yhb7588@naver.com>
2023-05-08 09:46:36 +0900

    permission class 추가

--- capstone2/backend/mainfunc/urls.py
+++ capstone2/backend/mainfunc/urls.py
@@ -1,13 +1,11 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 router = DefaultRouter()
 router.register('post', views.PostViewSet) # 2개의 url패턴 생성
 
 # router.urls = url 패턴 리스트
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
 ]
--- capstone2/backend/mainfunc/views.py
+++ capstone2/backend/mainfunc/views.py
@@ -1,7 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
 from .serializers import PostSerializer
 from .models import Post
+from rest_framework.permissions import AllowAny
 
+# CRUD가 모두 들어간 API를 지원
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [AllowAny] #FIXME: 인증 적용
