6a6fc7b4d4b02a2277cf51d20f323d89945e3b4b
DeveloperYun <yhb7588@naver.com>
2023-05-08 11:46:32 +0900

    antd import

--- capstone2/frontend/src/index.js
+++ capstone2/frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Root from 'pages';
 import {BrowserRouter}  from 'react-router-dom';
+import "antd/dist/antd.min.css";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
