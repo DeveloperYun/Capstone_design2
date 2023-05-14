84159d9363dbf3167320aa8b92032eaff6a1983a
DeveloperYun <yhb7588@naver.com>
2023-05-14 22:26:12 +0900

    label 테이블 추가, post 테이블 수정

--- capstone2/backend/mainfunc/models.py
+++ capstone2/backend/mainfunc/models.py
@@ -1,17 +1,21 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
 
-class TImestampedModel(models.Model):
-    # 기본 필드값
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+# cf. article : postImage = 1 : N
 
-    class Meta:
-        abstract = True
+#Label 테이블 -- 유저(외래키), 레이블
+class Label(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
+    label_name = models.CharField(max_length=50, default='')
 
-class Post(TImestampedModel):
-    label = models.CharField(max_length=50, default='')
-    images = models.ImageField(upload_to='images/%Y/%m/%d', blank=True)
+#Post 테이블 -- 레이블(외래키), 이미지
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
+    label = models.ForeignKey(Label, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/%Y/%m/%d', blank=True, null=True)
 
-    def __str__(self):
-        return f'{self.label}'
+    # def __str__(self):
+    #     return f'{self.label}'
 
