402670699c462208082e168061459692dc2b8718
DeveloperYun <yhb7588@naver.com>
2023-05-14 22:26:33 +0900

    시리얼라이저 수정

--- capstone2/backend/mainfunc/serializers.py
+++ capstone2/backend/mainfunc/serializers.py
@@ -1,14 +1,53 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Post# PostImage
+from rest_framework import serializers
+from .models import Post, Label
+from django.contrib.auth import get_user_model
 
-# class PostImageSerializer(ModelSerializer): #이미지 직렬화
-#     class Meta:
-#         model = PostImage
-#         fields = ['image']
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = '__all__'
 
-class PostSerializer(ModelSerializer):
-    # post에서 이미지를 한번에 입력받아서 저장할 것이기 때문.
+# label : Post = 1 : N
+# article : postImage = 1 : N
+# post = article
+
+# views.py 에서 호출한 시리얼라이저_클래스는 PostSerializer 
+class PostSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(read_only=True)
+    image = serializers.ImageField(use_url=True)
+
+    #label_name = serializers.CharField(source='label.label_name')  # Add this line
+
+    # def get_images(self, obj):
+    #     image = obj.post_set.all()
+    #     return PostSerializer(instance=image, many=True).data
     
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = ['author','image']
+
+    # def create(self, validated_data):
+    #     instance = Label.objects.create(**validated_data)
+    #     image_set = self.context['request'].FILES
+    #     for image_data in image_set.getlist('image'):
+    #         Post.objects.create(label=instance, image=image_data)
+    #     return instance
+
+class LabelSerializer(serializers.ModelSerializer):
+
+    def get_images(self, obj):
+        image = obj.image.all() 
+        return PostSerializer(instance=image, many=True, context=self.context).data
+
+
+    class Meta:
+        model = Label
+        fields = ['label_name']
+
+    def create(self, validated_data):
+        instance = Label.objects.create(**validated_data)
+        image_set = self.context['request'].FILES
+        for image_data in image_set.getlist('image'):
+            Post.objects.create(label=instance, image=image_data)
+        return instance 
+
