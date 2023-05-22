26d043c17aa8f9b7f97095bc8319eba85fa135a5
DeveloperYun <yhb7588@naver.com>
2023-05-22 18:34:56 +0900

    로그인, 회원가입 버튼 추가

--- capstone2/frontend/src/pages/Home.js
+++ capstone2/frontend/src/pages/Home.js
@@ -9,9 +9,25 @@ function Home(){
     const handleClick = () => {
         history("/posts/new");
     }
+
+    const handleSignUp = () => {
+        history("/accounts/signup");
+      };
+    
+      const handleLogin = () => {
+        history("/accounts/login");
+      };
+    
     return (
         <AppLayout>
+            
             <Button type="primary" onClick={handleClick}>데이터 셋 준비</Button>
+            <Button type="secondary" onClick={handleSignUp}>
+                Sign Up
+            </Button>
+            <Button type="secondary" onClick={handleLogin}>
+                Log In
+            </Button>
             <PostList />
         </AppLayout>
     );
