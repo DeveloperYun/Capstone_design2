417b67ea21ac905a12442126b15b747f08e5f35a
DeveloperYun <yhb7588@naver.com>
2023-05-27 17:57:04 +0900

    학습하기 버튼 분리를 위한 팝업창 추가 및 기타 작업

--- capstone2/frontend/src/pages/Home.js
+++ capstone2/frontend/src/pages/Home.js
@@ -1,36 +1,111 @@
-import React from "react";
-import PostList from "components/PostList";
-import AppLayout from "components/AppLayout";
-import { Button } from "antd";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "./Home.css";
+import Axios from "axios";
+import { useAppContext } from "store";
 
-function Home(){
-    const history = useNavigate();
+function Home() {
+  const [dataset, setDataset] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const history = useNavigate();
 
-    const handleClick = () => {
-        history("/posts/new");
-    }
+  const {
+    store: { jwtToken, username },
+  } = useAppContext();
 
-    const handleSignUp = () => {
-        history("/accounts/signup");
+  const handleSignUp = () => {
+    history("/accounts/signup");
+  };
+
+  const handleLogin = () => {
+    history("/accounts/login");
+  };
+
+  const handleFunc = () => {
+    history("/posts/new");
+  };
+
+  const handleTrain = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleDatasetChange = (event) => {
+    setDataset(event.target.value);
+  };
+
+  const handleTrainSubmit = () => {
+    const data = {
+      username: username,
+      dataset: dataset,
     };
-    
-    const handleLogin = () => {
-        history("/accounts/login");
-    };
-    
-    return (
-        <AppLayout>
-            <Button type="primary" onClick={handleClick}>데이터 셋 준비</Button>
-            <Button type="secondary" onClick={handleSignUp}>
-                Sign Up
-            </Button>
-            <Button type="secondary" onClick={handleLogin}>
-                Log In
-            </Button>
-            <PostList />
-        </AppLayout>
-    );
+
+    console.log(">> ",data)
+    Axios.post("http://localhost:8000/train/", data, {
+      headers: {
+        Authorization: `JWT ${jwtToken}`,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        console.log(response.data);
+        setIsModalOpen(false);
+        history("/train");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  return (
+    <div className="landing-page">
+      <header>
+        <div className="container">
+          <a href="#" className="logo">
+            Your <b>Website</b>
+          </a>
+          <ul className="links">
+            <li href="#">Home</li>
+            <li>About Us</li>
+            <li>Info</li>
+            <li onClick={handleSignUp}>Sign Up</li>
+            <li onClick={handleLogin}>Log In</li>
+          </ul>
+        </div>
+      </header>
+      <div className="content">
+        <div className="container">
+          <div className="info">
+            <h1>Looking For Inspiration</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus odit nihil ullam nesciunt quidem
+              iste, Repellendus odit nihil
+            </p>
+            <button onClick={handleFunc}>바로가기</button>
+            <button onClick={handleTrain}>학습하기</button>
+          </div>
+          <div className="image">
+            <img src="https://i.postimg.cc/65QxYYzh/001234.png" alt="Landing Page Image" />
+          </div>
+        </div>
+      </div>
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <h2>학습시킬 데이터 셋을 입력하세요</h2>
+            <input type="text" value={dataset} onChange={handleDatasetChange} placeholder="Enter dataset" />
+            <div className="modal-buttons">
+              <button onClick={handleTrainSubmit}>Submit</button>
+              <button onClick={handleModalClose}>Cancel</button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Home;
--- capstone2/frontend/src/pages/index.js
+++ capstone2/frontend/src/pages/index.js
@@ -3,16 +3,16 @@ import AppLayout from "components/AppLayout";
 import { Route, Routes } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
-import Main from "./Main";
 import AccountRoutes from "./accounts";
 import PostNew from "./PostNew";
+import Train from "./train";
 
 function Root(){
     return (
         <>
             <Routes>
-                <Route path="/" element={<Main />}/>
-                <Route path="/home" element={<Home />}/>
+                <Route path="/" element={<Home />}/>
+                <Route path="/train" element={<Train />}/>
                 <Route path="/about" element={<About />}/>
                 <Route path="/posts/new" element={<PostNew />}/>
                 <Route path="/accounts/*" element={<AccountRoutes />} />
