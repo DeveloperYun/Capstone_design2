041f65cdb615e30ae3e47ec9b8312d8506f69f24
sejin1129 <113009722+sejin1129@users.noreply.github.com>
2023-06-12 18:39:40 +0900

    Delete Capstone_design2/Capstone_design2-main directory

--- Capstone_design2/Capstone_design2-main/frontend/src/pages/PostNew.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import PostNewForm from "components/PostNewForm";
-import "./PostNew.scss";
-
-export default function PostNew() {
-  return (
-        <PostNewForm />
-  );
-}
